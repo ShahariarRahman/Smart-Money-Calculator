9e409a0dd74a0c2e37652d8fcbad574ed9e2c7e4
ShahariarRahman <shahariarrahman98@gmail.com>
2022-02-17 20:04:03 +0600

    js-2

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
 function getExpenses() {
-
+    let totalExpenses = 0;
+    for (const expense of arguments) {
+        let expenseField = document.getElementById(expense + '-expenses');
+        let expenseValue = parseFloat(expenseField.value);
+        totalExpenses = totalExpenses + expenseValue;
+        expenseField.value = '';
+    }
+    return totalExpenses;
 };
-document.getElementById('expenses-btn').addEventListener('click', function () {
-    const totalIncomeField = document.getElementById('total-income');
-    const totalIncome = totalIncomeField.value;
-    const foodExpensesField = document.getElementById('food-expenses');
-    const foodExpenses = foodExpensesField.value;
-    const rentExpensesField = document.getElementById('rent-expenses');
-    const rentExpenses = rentExpensesField.value;
-    const clothesExpensesField = document.getElementById('clothes-expenses');
-    const clothesExpenses = clothesExpensesField.value;
+function getIncome(income) {
+    const incomeField = document.getElementById(income);
+    const incomeValue = parseFloat(incomeField.value);
+    incomeField.value = '';
+    return incomeValue;
+}
 
-    const totalMontlyCost = parseFloat(foodExpenses) + parseFloat(rentExpenses) + parseFloat(clothesExpenses);
-
+function expensesUpdate() {
+    const totalMontlyCost = getExpenses('food', 'rent', 'clothes');
     const totalExpensesElement = document.getElementById('total-expenses');
     totalExpensesElement.innerText = totalMontlyCost;
+    return totalMontlyCost;
+}
+function balanceUpdate() {
+    const totalIncome = getIncome('total-income');
+
+    const totalMontlyCost = expensesUpdate();
 
     const balance = totalIncome - totalMontlyCost;
     const totalBalance = document.getElementById('total-balance');
     totalBalance.innerText = balance;
+}
 
-    foodExpensesField.value = '';
-    rentExpensesField.value = '';
-    clothesExpensesField.value = '';
-    totalIncomeField.value = '';
+
+document.getElementById('expenses-btn').addEventListener('click', function () {
+    balanceUpdate();
 });
 
 document.getElementById('saving-btn').addEventListener('click', function () {
