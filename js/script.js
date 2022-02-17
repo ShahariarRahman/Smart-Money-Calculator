1f633ddc438917062a6f64ee7b346856d30b6457
ShahariarRahman <shahariarrahman98@gmail.com>
2022-02-17 20:05:43 +0600

    js 80%

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Function:: Returns Summation of Expenses:
 function getExpenses() {
     let totalExpenses = 0;
     for (const expense of arguments) {
@@ -8,63 +9,66 @@ function getExpenses() {
     }
     return totalExpenses;
 };
+// Function:: Returns Income:
 function getIncome(income) {
     const incomeField = document.getElementById(income);
     const incomeValue = parseFloat(incomeField.value);
     incomeField.value = '';
     return incomeValue;
 }
-function updateExpenses() {
-    const totalExpenses = getExpenses('food', 'rent', 'clothes');
-    if (totalExpenses > 0) {
-        const expensesElement = document.getElementById('total-expenses');
-        expensesElement.innerText = totalExpenses;
-        return totalExpenses;
-    }
-    else {
-        // give error massage
-    }
 
-}
-
+// Function:: Update the Total Expenses and Balance:
 function updateBalanceAndExpences() {
+    const totalExpenses = getExpenses('food', 'rent', 'clothes');
+    const expensesElement = document.getElementById('total-expenses');
+    expensesElement.innerText = totalExpenses;
+
     const income = getIncome('total-income');
-
-    const totalExpenses = updateExpenses();
-
     const balance = income - totalExpenses;
-    const totalBalance = document.getElementById('total-balance');
-    totalBalance.innerText = balance;
-    return { income, totalExpenses, balance };
+    const balanceElement = document.getElementById('total-balance');
+    balanceElement.innerText = balance;
 }
+// Function:: Returns HTMl InnerText in Float:
+function getInnerElement(id) {
+    const element = document.getElementById('total-' + id);
+    const value = parseFloat(element.innerText);
+    return value;
+}
+// Function:: Returns Input Field's value in Float:
+function getFieldValue(id) {
+    const savingPercentageField = document.getElementById(id);
+    const savingPercentage = parseFloat(savingPercentageField.value);
+    savingPercentageField.value = '';
+    return savingPercentage;
+}
+// Function:: Update HTML InnerText:
+function setInnerElement(id, value) {
+    const totalSavingElement = document.getElementById(id);
+    totalSavingElement.innerText = value.toFixed(2);
+}
+// Function:: Update Saving Amount and Remaining Balance:
+function updateSaveAndRemining() {
+    const savingPercentage = getFieldValue('saving-percentage');
+    const totalExpenses = getInnerElement('expenses');
+    const totalBalance = getInnerElement('balance');
 
+    let totalIncome = totalExpenses + totalBalance;
+    const income = getIncome('total-income');
+    if (isNaN(income) != true) {
+        totalIncome = income;
+    }
+    const savingAmount = (totalIncome * savingPercentage) / 100;
 
+    setInnerElement('total-saving', savingAmount);
+
+    const remainingBalance = totalIncome - (totalExpenses + savingAmount);
+    setInnerElement('total-remaining', remainingBalance);
+}
+// Event handler:: for Calculate Button:
 document.getElementById('expenses-btn').addEventListener('click', function () {
     updateBalanceAndExpences();
 });
-
+// Event handler:: for Save Button:
 document.getElementById('saving-btn').addEventListener('click', function () {
-    const array = updateBalanceAndExpences();
-    const savingPercentageField = document.getElementById('saving-percentage');
-    const savingPercentage = parseFloat(savingPercentageField.value);
-
-
-    const totalIncome = array.income;
-
-    const savingAmount = (totalIncome * savingPercentage) / 100;
-
-    const totalSavingElement = document.getElementById('total-saving');
-    totalSavingElement.innerText = savingAmount;
-
-
-    const totalExpensesElement = document.getElementById('total-expenses');
-    const totalExpenses = parseFloat(totalExpensesElement.innerText)
-
-    const remainingBalance = totalIncome - (totalExpenses + savingAmount);
-    const totalRemainingElement = document.getElementById('total-remaining');
-    totalRemainingElement.innerText = remainingBalance;
-
-
-
-    savingPercentageField.value = '';
+    updateSaveAndRemining();
 });
